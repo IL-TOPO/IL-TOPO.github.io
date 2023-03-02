b880499c6630d5c19ed31660c2800f7fce914dd2
IL-TOPO <ganatale86@gmail.com>
2023-03-02 13:09:11 -0300

    se agrega actualización en function.js

--- functions.js
+++ functions.js
@@ -10,21 +10,11 @@ document.getElementById("mail").addEventListener('click', function() {
 document.getElementById("linkedin").addEventListener('click', function() {
     const enlace = document.getElementById('linkedin');
     window.open('https://www.linkedin.com', '_blank', 'noopener', 'noreferrer');    
-    console.log("██████╗░░█████╗░███╗░░░███╗██████╗░██╗███╗░░██╗░█████╗░")
-    console.log("██╔══██╗██╔══██╗████╗░████║██╔══██╗██║████╗░██║██╔══██╗")
-    console.log("██████╦╝███████║██╔████╔██║██████╦╝██║██╔██╗██║██║░░██║")
-    console.log("██╔══██╗██╔══██║██║╚██╔╝██║██╔══██╗██║██║╚████║██║░░██║")
-    console.log("██████╦╝██║░░██║██║░╚═╝░██║██████╦╝██║██║░╚███║╚█████╔╝")
-    console.log("╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░╚═╝╚═╝░░╚══╝░╚════╝░")
 })
 
 document.getElementById("mapa").addEventListener('click', function() {
     const enlace = document.getElementById('mapa');
     window.open('https://www.google.com/maps/place/93100+Caltanissetta,+Italia/@37.485763,14.0218291,13z/data=!3m1!4b1!4m5!3m4!1s0x1310c261e52ae8a3:0x455ddc54ef3d967f!8m2!3d37.4901115!4d14.0628928', '_blank', 'noopener', 'noreferrer');
-    console.log("░█▀▀█ ─█▀▀█ ░█▀▄▀█ ░█▀▀█ ▀█▀ ░█▄─░█ ░█▀▀▀█ ")
-    console.log("░█▀▀▄ ░█▄▄█ ░█░█░█ ░█▀▀▄ ░█─ ░█░█░█ ░█──░█ ")
-    console.log("░█▄▄█ ░█─░█ ░█──░█ ░█▄▄█ ▄█▄ ░█──▀█ ░█▄▄▄█")
-    
 })
 
 
